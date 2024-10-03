0cfb2e69786bb1a42e6a839437acb1db931c53ab
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2024-10-03 11:29:08 +0530

    Update scripts

---
+++ scripts/file-arrays-used-in-games.js
@@ -0,0 +1,28 @@
+// script for listing classwise file arrays to be used across all games
+
+// Function to get URL parameter by name
+function getQueryParam(name) {
+   const urlParams = new URLSearchParams(window.location.search);
+   return urlParams.get(name);
+}
+
+// Get the grade from the URL, default to 6 if missing or invalid
+const grade = parseInt(getQueryParam('grade')) || 6;
+const validGrade = [6, 7, 8].includes(grade) ? grade : 6;
+
+// fetch different files depending on grade
+let gradeArray;
+switch (validGrade) {
+   case 6:
+      gradeArray = ['kips-6-ch-1', 'kips-6-ch-2', 'kips-6-ch-3', 'kips-6-ch-4'];
+      break;
+   case 7:
+      gradeArray = ['kips-7-ch-1', 'kips-7-ch-2', 'kips-7-ch-3', 'kips-7-ch-4'];
+      break;
+   case 8:
+      gradeArray = ['kips-8-ch-1', 'kips-8-ch-2', 'between-8-ch-1', 'between-8-ch-2'];
+      break;
+   default:
+      gradeArray = [];
+      console.log("Invalid grade");
+}
